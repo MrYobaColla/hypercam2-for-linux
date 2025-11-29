338dce8e7f7e5f14a53cdc321af0a59a128dae8d
Mr. Acyc <kastian_kostianich2007@mail.ru>
2025-11-29 23:21:13 +0300

    Add files via upload

---
+++ include.h
@@ -0,0 +1,15 @@
+#include <gtk/gtk.h>
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <gst/audio/audio.h>
+#include <gdk/gdkx.h>
+#include <gdk/gdkkeysyms.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+
